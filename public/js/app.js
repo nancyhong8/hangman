892caf05b163dfec5bb32e615282fe986ced8fae
nancyhong8 <hong.na@husky.neu.edu>
2017-02-23 15:41:51 -0500

    need to do server

--- public/js/app.js
+++ public/js/app.js
@@ -18,12 +18,32 @@ hangmanApp.directive('draggables', function dragInstructions() {
 hangmanApp.controller('hangmanController', function hangmanController($scope, $modal) {
 
 
-    $scope.init = init;
 
-        function init() {
+    function init() {
         document.location.reload();
     }
+    $scope.init = init;
 
+    /* When the user clicks on the button,
+     toggle between hiding and showing the dropdown content */
+    function myFunction() {
+        document.getElementById("myDropdown").classList.toggle("show");
+    }
+
+// Close the dropdown menu if the user clicks outside of it
+    window.onclick = function(event) {
+        if (!event.target.matches('.dropbtn')) {
+
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }
 
     var wrongLetter = [];
     var space = [];
@@ -151,6 +171,7 @@ hangmanApp.controller('hangmanController', function hangmanController($scope, $m
         })
     }
 
+    $scope.openLost = openLost;
     $scope.arrayToString = function(string){
         if (string != null) {
             return string.join(", ");
